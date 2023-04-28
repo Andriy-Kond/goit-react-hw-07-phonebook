6f249fe18e4b02d2f1f41c8b7aeaa0c33dcd6593
Andrii <107649409+Andriy-Kond@users.noreply.github.com>
2023-04-28 12:10:54 +0300

    option 01

--- src/store/sliceAsyncThunk.js
+++ src/store/sliceAsyncThunk.js
@@ -1,6 +1,6 @@
 import { addContact, deleteContact, fetchContacts } from '../services/fetch';
 
-const { createSlice } = require('@reduxjs/toolkit');
+const { createSlice, isAnyOf } = require('@reduxjs/toolkit');
 
 const initialState = {
   stateContacts: [],
@@ -41,10 +41,10 @@ const deleteHandleFulfilled = (state, action) => {
 };
 // * /Для скорочення коду extraReducers
 
-// // Оптимізація для скорочення назви у масиві функції isAnyOf([], callback)
-// // Створюємо масив з імен і додаємо до кожного якийсь статус, в залежності від ситуації
-// const namesArr = [fetchContacts, addContact, deleteContact];
-// const addStatusToName = status => namesArr.map(name => name[status]);
+// Оптимізація для скорочення назви у масиві функції isAnyOf([], callback)
+// Створюємо масив з імен і додаємо до кожного якийсь статус, в залежності від ситуації
+const namesArr = [fetchContacts, addContact, deleteContact];
+const addStatusToName = status => namesArr.map(name => name[status]);
 
 const sliceAsyncThunk = createSlice({
   name: 'fetchContacts',
@@ -53,47 +53,47 @@ const sliceAsyncThunk = createSlice({
   // ~ Новий метод: 'builder callback' notation
   extraReducers: builder => {
     builder
-      .addCase(fetchContacts.pending, handlePending) // переніс у isAnyOf
+      // .addCase(fetchContacts.pending, handlePending) // переніс у isAnyOf
       .addCase(fetchContacts.fulfilled, fetchHandleFulfilled)
-      .addCase(fetchContacts.rejected, handleRejected) // переніс у isAnyOf
+      // .addCase(fetchContacts.rejected, handleRejected) // переніс у isAnyOf
 
-      .addCase(addContact.pending, handlePending) // переніс у isAnyOf
+      // .addCase(addContact.pending, handlePending) // переніс у isAnyOf
       .addCase(addContact.fulfilled, addHandleFulfilled)
-      .addCase(addContact.rejected, handleRejected) // переніс у isAnyOf
+      // .addCase(addContact.rejected, handleRejected) // переніс у isAnyOf
 
-      .addCase(deleteContact.pending, handlePending) // переніс у isAnyOf
+      // .addCase(deleteContact.pending, handlePending) // переніс у isAnyOf
       .addCase(deleteContact.fulfilled, deleteHandleFulfilled)
-      .addCase(deleteContact.rejected, handleRejected); // переніс у isAnyOf
-    // isAnyOf працює як логічне АБО. Приймає масив.
-    // Читається так: якщо хтось з перелічених з масиву, то роби колбек після коми
+      // .addCase(deleteContact.rejected, handleRejected) // переніс у isAnyOf
+      // isAnyOf працює як логічне АБО. Приймає масив.
+      // Читається так: якщо хтось з перелічених з масиву, то роби колбек після коми
 
-    // ??? Чомусь не працює!
-    // ! Uncaught TypeError: matcher is not a function
-    // .addMatcher(
-    //   isAnyOf(
-    //     [fetchContacts.pending, addContact.pending, deleteContact.pending],
-    //     handlePending
-    //   )
-    // );
-    // ??? Чомусь не працює!
-    // ! Uncaught TypeError: matcher is not a function
-    // .addMatcher(
-    //   isAnyOf(
-    //     [
-    //       fetchContacts.rejected,
-    //       addContact.rejected,
-    //       deleteContact.rejected,
-    //     ],
-    //     handleRejected
-    //   )
-    // )
+      // ??? Чомусь не працює!
+      // ! Uncaught TypeError: matcher is not a function
+      // .addMatcher(
+      //   isAnyOf(
+      //     [fetchContacts.pending, addContact.pending, deleteContact.pending],
+      //     handlePending
+      //   )
+      // );
+      // ??? Чомусь не працює!
+      // ! Uncaught TypeError: matcher is not a function
+      // .addMatcher(
+      //   isAnyOf(
+      //     [
+      //       fetchContacts.rejected,
+      //       addContact.rejected,
+      //       deleteContact.rejected,
+      //     ],
+      //     handleRejected
+      //   )
+      // )
 
-    // ??? З цим записом чомусь працює!!!
-    // .addMatcher(isAnyOf(...addStatusToName('pending'), handlePending));
+      // ??? З цим записом чомусь працює!!!
+      .addMatcher(isAnyOf(...addStatusToName('pending'), handlePending))
 
-    // ??? А цей запис чомусь не працює!
-    // ! Uncaught TypeError: Cannot read properties of undefined (reading 'payload') --- sliceAsyncThunk.js:18 - тобто йде виклик з rejected, коли його йти не має.
-    // .addMatcher(isAnyOf(...addStatusToName('rejected'), handleRejected));
+      // ??? А цей запис чомусь не працює!
+      // ! Uncaught TypeError: Cannot read properties of undefined (reading 'payload') --- sliceAsyncThunk.js:18 - тобто йде виклик з rejected, коли його йти не має.
+      .addMatcher(isAnyOf(...addStatusToName('rejected'), handleRejected));
   },
 });
 
