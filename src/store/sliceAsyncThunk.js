34ee1e5d467cacb7cfc68baf4c0a002f2b0b0d7a
Andrii <107649409+Andriy-Kond@users.noreply.github.com>
2023-04-28 15:27:18 +0300

    .addMatcher

--- src/store/sliceAsyncThunk.js
+++ src/store/sliceAsyncThunk.js
@@ -69,31 +69,28 @@ const sliceAsyncThunk = createSlice({
 
       // ??? Чомусь не працює!
       // ! Uncaught TypeError: matcher is not a function
-      // .addMatcher(
-      //   isAnyOf(
-      //     [fetchContacts.pending, addContact.pending, deleteContact.pending],
-      //     handlePending
-      //   )
-      // );
+      .addMatcher(
+        isAnyOf([
+          fetchContacts.pending,
+          addContact.pending,
+          deleteContact.pending,
+        ]),
+        handlePending
+      )
       // ??? Чомусь не працює!
       // ! Uncaught TypeError: matcher is not a function
-      // .addMatcher(
-      //   isAnyOf(
-      //     [
-      //       fetchContacts.rejected,
-      //       addContact.rejected,
-      //       deleteContact.rejected,
-      //     ],
-      //     handleRejected
-      //   )
-      // )
+      .addMatcher(
+        isAnyOf([
+          fetchContacts.rejected,
+          addContact.rejected,
+          deleteContact.rejected,
+        ]),
+        handleRejected
+      );
 
-      // ??? З цим записом чомусь працює!!!
-      .addMatcher(isAnyOf(...addStatusToName('pending'), handlePending))
-
-      // ??? А цей запис чомусь не працює!
-      // ! Uncaught TypeError: Cannot read properties of undefined (reading 'payload') --- sliceAsyncThunk.js:18 - тобто йде виклик з rejected, коли його йти не має.
-      .addMatcher(isAnyOf(...addStatusToName('rejected'), handleRejected));
+    // Так працює:
+    // .addMatcher(isAnyOf(...addStatusToName('pending')), handlePending)
+    // .addMatcher(isAnyOf(...addStatusToName('rejected')), handleRejected);
   },
 });
 
